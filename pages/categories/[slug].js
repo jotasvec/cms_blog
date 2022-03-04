cc3bfb033b74aec1d98c38f37629696eca17cba3
jonathan svec <jotasvec@gmail.com>
2022-03-04 09:25:04 +0100

    adding loader

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -1,28 +1,36 @@
 import React from 'react'
+import { useRouter } from 'next/router';
+
 import { getCategories, getPostByCategories } from "../../services";
 
-import { PostCard, Categories } from "../../components";
+import { PostCard, Categories, Loader } from "../../components";
 
 const postsCategories = ({posts}) => {
-  return (
-    <div className='container mx-auto px-10 mb-8'>
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-            <div className="col-span-1 lg:col-span-8">
-                { 
-                    posts.map((post, index) => (
-                        <PostCard post={post.node} key={index} />
-                    ))
-                }
-            </div>
-            {/* sidebar */}
-            <div className="col-span-1 lg:col-span-4">
-                <div className="lg:sticky relative top-8">
-                    <Categories />
+
+    const router = useRouter();
+
+    if (router.isFallback){
+        return <Loader />
+    }
+    return (
+        <div className='container mx-auto px-10 mb-8'>
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+                <div className="col-span-1 lg:col-span-8">
+                    { 
+                        posts.map((post, index) => (
+                            <PostCard post={post.node} key={index} />
+                        ))
+                    }
+                </div>
+                {/* sidebar */}
+                <div className="col-span-1 lg:col-span-4">
+                    <div className="lg:sticky relative top-8">
+                        <Categories />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default postsCategories;
